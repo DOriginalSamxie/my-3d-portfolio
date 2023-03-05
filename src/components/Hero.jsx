52deaa7fcc9571d70f4d42857f6ce20508341ef2
DOriginalSamxie <samadrufai@gmail.com>
2023-03-05 17:44:15 +0100

    mouse movement implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -20,11 +20,28 @@ const Hero = () => {
             As a React developer, I specialize in creating highly dynamic and
             responsive user
             <br className='sm:block hidden' /> interfaces using the React
-            JavaScript library. With expertise in JavaScript, HTML, and CSS.
+            {/* JavaScript library. With expertise in JavaScript, HTML, and CSS. */}
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1 '
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,14 +1,31 @@
-import { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
 import CanvasLoader from '../Loader'
-const Computers = () => {
+
+const Computers = (props) => {
+  const mesh = useRef(null)
+
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <mesh>
+    <mesh {...props} ref={mesh}>
       <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
@@ -16,7 +33,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameLoop='demand'
+      frameloop='demand'
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
